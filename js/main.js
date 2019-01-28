8f77008e36ee00e619bf7d0f397dc8ed36e517fb
Kudakwashe24 <kudakwashe755@gmail.com>
2019-01-28 13:29:04 +0200

    responsiveness

--- js/main.js
+++ js/main.js
@@ -65,3 +65,17 @@ function addCart(name, price) {
 //add a quantity to cart items
 //be able to increase and decrease the quantity 
 //REMEMBER the cart total must update as the quantity changes
+
+// JQuery counter codepen
+// Changed the class name, assisted by Abdul
+$('.number').each(function () {
+    var size = $(this).text().split(".")[1] ? $(this).text().split(".")[1].length : 0;
+    $(this).prop('Counter', 0).animate({
+       Counter: $(this).text()
+    }, {
+       duration: 5000,
+       step: function (func) {
+          $(this).text(parseFloat(func).toFixed(size));
+       }
+    });
+ });
